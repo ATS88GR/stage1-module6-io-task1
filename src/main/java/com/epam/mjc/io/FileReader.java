f3a3c3b9c9a099598e897b9f68dc4d5efe9c42dd
ATS88GR <turok.artyom@gmail.com>
2024-01-30 23:43:50 +0300

    add m6, t1 io

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        StringBuilder fileToStr = new StringBuilder();
+
+        try (FileInputStream fin = new FileInputStream(file)) {
+            int i = -1;
+            while ((i = fin.read()) != -1) {
+
+                fileToStr.append((char) i);
+            }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+        String [] spitStr = fileToStr.toString().split("\n");
+
+        Profile profile = new Profile();
+        profile.setName(spitStr[0].split(" ") [1]);
+        profile.setAge(Integer.parseInt(spitStr[1].split(" ") [1]));
+        profile.setEmail(spitStr[2].split(" ") [1]);
+        profile.setPhone(Long.parseLong(spitStr[3].split(" ")[1]));
+
+        return profile;
     }
 }
